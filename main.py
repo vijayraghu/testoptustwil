299674e79072cfce2385ee49f6986c46a741adcc
vijayraghu <vijayasarathypr@gmail.com>
2017-11-20 16:35:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ registered_users = {"+914444461497": "Paul",
                    "+914444462805": "Peter"
 }
 # Adjust the hints for improved Speech to Text
-hints = "1 one first, 2 two second, 3 three third, 4 four fourth, 5 five fifth, 6 six sixth, 7 seven seventh, 8 eight eighth,9 nine ninth, 10 ten tenth, Paul Pal Pole Pall Pale, Peter Petre Pete, january, february, march, april, may, june, july, august, september, october, november, december"
+hints = "1 one first, 2 two second, 3 three third, 4 four fourth, 5 five fifth, 6 six sixth, 7 seven seventh, 8 eight eighth,9 nine ninth, 10 ten tenth, account acount akount, Paul Pal Pole Pall Pale, Peter Petre Pete, january, february, march, april, may, june, july, august, september, october, november, december"
 
 app = Flask(__name__)
 
