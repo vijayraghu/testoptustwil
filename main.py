0160336feebf3c98b57225042c38414a101ceffc
vijayraghu <vijayasarathypr@gmail.com>
2018-07-31 13:50:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,28 +8,36 @@ from flask import Flask, request, Response, make_response, jsonify, url_for
 from contextlib import closing
 # Twilio Helper Library
 from twilio.twiml.voice_response import VoiceResponse, Gather, Say, Dial
-import re
-import datetime
 # Google Text To Speech SDK
 from google.oauth2 import service_account
 from google.cloud import texttospeech_v1beta1 as texttospeech
+# Dialogflow V2 SDK
+import dialogflow
+import re
+import datetime
 
-# Declare global variables
-apiai_client_access_key = os.environ["APIAPI_CLIENT_ACCESS_KEY"]
-apiai_url = "https://api.api.ai/v1/query"
-apiai_querystring = {"v": "20150910"}
+#####
+##### Declare Global variables
+#####
+# Setting Google ID - Read env data
+project_id = os.environ["DIALOGFLOW_PROJECT_ID"]
+#Setting Google authorization credentials -  Read env data
+credentials_dgf = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+#apiai_url = "https://api.api.ai/v1/query"
+#apiai_querystring = {"v": "20150910"}
 
-# Setup hints for better speech recognition
+# Setup hints (speech contexts) for better speech recognition
 hints = "1,2,3,4,5,6,7,8,9,0, 1 one first, 2 two second, 3 three third, 4 four fourth, 5 five fifth, 6 six sixth, 7 seven seventh, 8 eight eighth,9 nine ninth, 10 ten tenth, 0 zero o, account acount akount, mobile, roaming, top up topup,channels channel,tv TV, broadband broad band, fetch, extension, iphone, cable, recharge, recharging, optus Optus, Hey, EPL, English premier league, streaming, premier league"
 
 app = Flask(__name__)
 
-@app.route('/start', methods=['GET','POST'])
-def start():
-	caller_phone_number = request.values.get('From')
-	user_id = request.values.get('CallSid')
-	twilio_asr_language = request.values.get('twilio_asr_language', 'en-AU')
-	apiai_language = request.values.get('apiai_language', 'en')
+# Receive call from Twilio with paramters
+@app.route('/welcome', methods=['GET','POST'])
+def welcome():
+	cli = request.values.get('From')
+	call_id = request.values.get('CallSid')
+	asr_lang = request.values.get('asr_lang', 'en-AU')
+	lang_code = request.values.get('lang_code', 'en')
 	hostname = request.url_root
 	
 	# Check for HOOP (hours of operations)
@@ -37,22 +45,24 @@ def start():
 	end = datetime.time(23, 59)
 	timestamp = datetime.datetime.now().time()
 	resp = VoiceResponse()
+	
 	if (end <= timestamp >= start):
 		# If call time not within hours of operation, play appropriate prompt and transfer to general line
-		values = {"text": 'Our office hours are from 08:30 AM till 18:00 PM on weekdays. Kindly hold while we transfer your call to our general assistance line and a customer service representative will assist you'}
+		values = {"text": 'Hi! The Care For me team is currently closed, the team is opened from 8:30 till 6 P M weekdays, please hold and I’ll transfer your call to the General Customer Service Number.'}
 		print 'In start: before Google TTS'
 		resp.play(hostname + 'goog_text2speech?' + qs)
 		print 'In start: after Google TTS'
 		resp.dial('+61280490603')
 		return str(resp)
+		
 	else:
 		# If call within office hours, triggering Dialogflow "Welcome" event
 		headers = {'authorization': 'Bearer ' + apiai_client_access_key, 
 			   'content-type': 'application/json'
 			  }
 		payload = {'event': {'name': 'Welcome'}, 
-			   'lang': apiai_language, 
-			   'sessionId': user_id
+			   'lang': lang_code, 
+			   'sessionId': call_id
 			  }
 		response = requests.request("POST", url=apiai_url, data=json.dumps(payload), headers=headers, params=apiai_querystring)
 		print response.text
@@ -65,7 +75,7 @@ def start():
 		values = {'prior_text': output_text}
 		qs = urllib.urlencode(values)
 		action_url = '/process_speech?' + qs
-		gather = Gather(input="speech", hints=hints, language=twilio_asr_language, speechTimeout="auto", action=action_url, method="POST")
+		gather = Gather(input="speech", hints=hints, language=asr_lang, speechTimeout="auto", action=action_url, method="POST")
 		
 		# Welcome prompt played to callers during office hours
 		values = {"text": output_text}
@@ -77,8 +87,8 @@ def start():
 		
 		# If user input is missing after welcome prompt (no speech input), redirect to collect speech input again
 		values = {'prior_text': output_text, 
-			  'twilio_asr_language': twilio_asr_language, 
-			  'apiai_language': apiai_language, 
+			  'asr_lang': asr_lang, 
+			  'lang_code': lang_code, 
 			  'SpeechResult': '', 
 			  'Confidence': 0.0
 			 }
@@ -87,16 +97,16 @@ def start():
 		resp.redirect(action_url)
 		print str(resp)
 		return str(resp)
-    
+		
 #####
 ##### Process Twilio ASR: "Speech to Text" to Dialogflow Intent analysis
 #####
 @app.route('/process_speech', methods=['GET', 'POST'])
 def process_speech():
-	caller_phone_number = request.values.get('From')
-	user_id = request.values.get('CallSid')
-	twilio_asr_language = request.values.get('twilio_asr_language', 'en-AU')
-	apiai_language = request.values.get('apiai_language', 'en')
+	cli = request.values.get('From')
+	call_id = request.values.get('CallSid')
+	asr_lang = request.values.get('asr_lang', 'en-AU')
+	lang_code = request.values.get('lang_code', 'en')
 	prior_text = request.values.get('prior_text', 'Prior text missing')
 	input_text = request.values.get('SpeechResult', '')
 	confidence = float(request.values.get('Confidence', 0.0))
@@ -111,13 +121,13 @@ def process_speech():
 	
 	if (confidence >= 0.0):
 		# Step 1: Call Dialogflow for intent analysis
-		intent_name, output_text, product_name, emp_id, intent_stage = apiai_text_to_intent(apiai_client_access_key, input_text, user_id, apiai_language)
+		intent_name, output_text, product_name, emp_id = dialogflow_text_to_intent(project_id, input_text, call_id, lang_code)
 		
 		# Step 2: Speech input processing by Twilio
 		values = {'prior_text': output_text}
-        	qs2 = urllib.urlencode(values)
+       		qs2 = urllib.urlencode(values)
         	action_url = '/process_speech?' + qs2
-        	gather = Gather(input="speech", hints=hints, language=twilio_asr_language, speechTimeout="auto", action=action_url, method="POST")
+        	gather = Gather(input="speech", hints=hints, language=asr_lang, speechTimeout="auto", action=action_url, method="POST")
         	values = {"text": output_text}
 		qs1 = urllib.urlencode(values)
 		print 'In-progress: Before Google tts'
@@ -148,14 +158,12 @@ def process_speech():
 		if intent_name != '' and product_name != '':
 			phone_number = getroutepoint(intent_name, product_name)
 			resp.dial(phone_number)
-			resp.redirect('/process_close')
-			
-		#if intent_name in ['billing_services_cartwright','sales_services_cartwright']:
+			resp.redirect('/process_close')	
 			
 		# If gather is missing (no speech input), redirect to process incomplete speech via Dialogflow
 		values = {'prior_text': output_text, 
-			  'twilio_asr_language': twilio_asr_language, 
-			  'apiai_language': apiai_language, 
+			  'asr_lang': asr_lang,
+			  'lang_code': lang_code, 
 			  'SpeechResult': '', 
 			  'Confidence': 0.0}
 		qs3 = urllib.urlencode(values)
@@ -168,7 +176,7 @@ def process_speech():
         	values = {"prior_text": output_text}
 		qs2 = urllib.urlencode(values)
 		action_url = "/process_speech?" + qs2
-		gather = Gather(input="speech", hints=hints, language=twilio_asr_language, speechTimeout="auto", action=action_url, method="POST")
+		gather = Gather(input="speech", hints=hints, language=asr_lang, speechTimeout="auto", action=action_url, method="POST")
 		values = {"text": output_text}
 		qs1 = urllib.urlencode(values)
 		print 'Before Google tts'
@@ -186,51 +194,53 @@ def process_speech():
 def process_close():
 	print 'in process_close'
 	
-	
-#####
-##### Google Dialogflow - Intent identification from text
-#####
-#@app.route('/apiai_text_to_intent', methods=['GET', 'POST'])
-def apiai_text_to_intent(apiapi_client_access_key, input_text, user_id, language):
-	print "Inside apiai_text_to_intent"
-	headers = {
-		'authorization': "Bearer " + apiapi_client_access_key, 
-		'content-type': "application/json"
-	}
-	payload = {'query': input_text, 
-		   'lang': language, 
-		   'sessionId': user_id
-		  }
-	response = requests.request("POST", url=apiai_url, data=json.dumps(payload), headers=headers, params=apiai_querystring)
-	output = json.loads(response.text)
-	print output
-	print json.dumps(output, indent=2)
-	
-	# Get values from Dialogflow
-	try:
-		intent_name = output['result']['metadata']['intentName']
-	except:
-		intent_name= ""
-	try:
-		product_name = output['result']['parameters']['optus_product']
-	except:
-		product_name= ""
-	try:
-		emp_id = output['result']['parameters']['employee_id']
-	except:
-		emp_id= ""	
-	try:
-		output_text = output['result']['fulfillment']['speech']
-	except:
-		output_text = ""
-	try:
-		intent_stage = output['result']['contexts']
-    	except:
-		intent_stage = "unknown"
-    	
-	return intent_name, output_text, product_name, emp_id, intent_stage
 
-# Get route point based on Intent and product#
+	
+
+#####
+##### Google Dialogflow V2 API - Intent identification from text
+#####
+#@app.route('/dialogflow_text_to_intent', methods=['GET', 'POST'])
+def dialogflow_text_to_intent(project_id, call_id, input_text, lang_code):
+
+	#Generate Google Dialogflow Credentials
+	service_account_info = json.loads(credentials_dgf)
+	credentials = service_account.Credentials.from_service_account_info(service_account_info)
+
+	session_client = dialogflow.SessionsClient(credentials=credentials)
+	session = session_client.session_path(project_id, call_id)
+	
+	for text in input_text:
+        	text_input = dialogflow.types.TextInput(text=text, language_code=lang_code)
+		query_input = dialogflow.types.QueryInput(text=text_input)
+		response = session_client.detect_intent(session=session, query_input=query_input)
+		output = json.loads(response.text)
+		print output
+		print json.dumps(output, indent=2)
+	
+		# Get values from Dialogflow
+		try:
+			intent_name = output['query_result']['intent']['display_name']
+		except:
+			intent_name= ""
+		try:
+			product_name = output['query_result']['parameters']['optus_product']
+		except:
+			product_name= ""
+		try:
+			emp_id = output['result']['parameters']['employee_id']
+		except:
+			emp_id= ""	
+		try:
+			output_text = output['query_result']['fulfillment_text']
+		except:
+			output_text = ""
+    	
+	return intent_name, output_text, product_name, emp_id
+
+#####
+##### Get route point based on Intent and product
+#####
 def getroutepoint(intent_name, product_name):
 	#Catch all exceptions
 	phone_number = "+61280490603"
@@ -292,6 +302,7 @@ def getroutepoint(intent_name, product_name):
 ##### Dialogflow fulfillment webhook
 #####
 @app.route('/webhook', methods=['POST'])
+# Receive the JSON request from Dialogflow
 def webhook():
 	req = request.get_json(silent=True, force=True)
 	print 'Request:'
@@ -302,53 +313,70 @@ def webhook():
 	r.headers['Content-Type'] = 'application/json'
 	return r
 
+# Get details from JSON 
 def processRequest(req):
-	caller_phone_number = request.values.get('From')
-	result = req.get('result')
-	metadata = result.get('metadata')
-	intentname = metadata.get('intentName')
-	parameters = result.get('parameters')
+	result = req.get('queryResult')
+	intentname = queryResult.intent.get('displayName')
+	metadata = queryResult.get('intent')
+	intentname = metadata.get('displayName')
+	parameters = queryResult.get('parameters')
 	actionname = parameters.get('action')
 	emp_id = parameters.get('employee_id')
 	product_name = parameters.get('optus_product')
-	resp = VoiceResponse()
 	
 	# Process employee number
-	if intentname == 'get_employee_number_cartwright_yes':
+	if intentname == 'get_employee_number_cartwright':
 		#Validate employee number
 		if (str(emp_id)[:2]) != '10':
-			speech = 'This is not a valid employee number. Kindly hold on while we connect you to one of our customer service agent'
+			fulfillmentText = 'Hmmm! That does not seem to be a valid employee number. Let me transfer you to one of my colleagues in the General Customer Service Team that can help you with your inquiry today.'
 		else:
-			speech = 'Thanks for providing your employee number. How can we help you today?'
+			employee_name = get_employee_name(emp_id)
+			fulfillmentText = 'Thanks ' + employee_name + ' for providing your employee number. Now how can we help you today?'
 	
 	# Get employee number again if user informs that employee id interpretation is incorrect
 	elif intentname == 'get_employee_number_cartwright_no':
-		speech = 'Please provide your employee number by speaking each digit individually'
+		fulfillmentText = 'Please provide your employee number by speaking each digit individually'
 	
     	# Transfer for Billing_services
     	elif intentname == 'billing_services_cartwright':
-		speech = 'Kindly hold on while we connect you to one of our customer service agent'
+		fulfillmentText = 'Ok. Let me transfer you to one of my colleagues that can help you with your Billing inquiry'
 	
     	# Transfer for Sales_services   
     	elif intentname == 'sales_services_cartwright':
-		speech = 'Kindly hold on while we connect you to one of our customer service agent'
+		fulfillmentText = 'Ok. Let me transfer you to one of my colleagues that can help you with your Sales inquiry'
 	
     	# Transfer for Tech_services
     	elif intentname == 'tech_services_cartwright':
-		speech = 'Kindly hold on while we connect you to one of our customer service agent'
+		fulfillmentText = 'Ok. Let me transfer you to one of my colleagues that can help you with your technical inquiry'
 			
     	# Transfer to General services if employee number is not provided
     	elif intentname == 'no_employee_number_cartwright':
-		speech = 'Kindly hold on while we connect you to one of our customer service agent'
+		fulfillmentText = 'Let me transfer you to one of my colleagues in the General Customer Service Team that can help you with your inquiry today.'
 		
 	# Catch all error/exception scenarios and transfer to General services
 	else:
-		speech = 'Kindly hold on while we connect you to one of our customer service agent'
+		fulfillmentText = 'Kindly hold on while we connect you to one of our customer service agent'
 	
-	return {'speech': speech, 'displayText': speech, 
-		'source': 'careforyou'
-	       }
+	return {'fulfillmentText': fulfillmentText, 'fulfillmentMessages': fulfillmentText, 'source': 'careforyou'}
+	
 	return res
+	
+# Helper function for getting employee name
+def get_employee_name(emp_id):
+	if emp_id = '1048350':
+		employee_name = 'Chris'
+	elif emp_id = '1048550':
+		employee_name = 'Mick'
+	elif emp_id = '1048550':
+		employee_name = 'Josh'
+	elif emp_id = '1058670':
+		employee_name = 'Paul'
+	elif emp_id = '1088430':
+		employee_name = 'Cameron'
+	else:
+		employee_name = ''
+		
+	return employee_name
 
 #####
 ##### Google Cloud Text to speech for Speech Synthesis
@@ -359,8 +387,8 @@ def goog_text2speech():
 	text = request.args.get('text', "Hello! Invalid request. Please provide the TEXT value")
 	
 	# Pre-process the text 
-	if len(text) == 0:
-		text = "We are experiencing technical difficulties at the moment. Please call back later."
+	#if len(text) == 0:
+		#text = "We are experiencing technical difficulties at the moment. Please call back later."
 	
 	# Adding space between numbers for better synthesis
 	if re.search(r'\b\d{1,16}\b', text):
@@ -371,25 +399,23 @@ def goog_text2speech():
 	effects_profile_id = 'telephony-class-application'
 	
 	#Setting credentials -  Read env data
-	credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+	#credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 	
 	#Generate Google TTS Credentials
-	service_account_info = json.loads(credentials_raw)
+	service_account_info = json.loads(credentials_dgf)
 	credentials = service_account.Credentials.from_service_account_info(service_account_info)
 		    
 	# Create Google Text-To-Speech client
     	client = texttospeech.TextToSpeechClient(credentials=credentials)
 	
-	#pass the text to be synthesized by Google Text-To-Speech
+	#Pass the text to be synthesized by Google Text-To-Speech
 	input_text = texttospeech.types.SynthesisInput(text=text)
 		
 	#Set the Google Text-To-Speech voice parameters
-    	voice = texttospeech.types.VoiceSelectionParams(language_code='en-AU', name='en-AU-Wavenet-B', ssml_gender=texttospeech.enums.SsmlVoiceGender.MALE)
+    	voice = texttospeech.types.VoiceSelectionParams(language_code='en-AU', name='en-AU-Wavenet-A', ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
 
 	#Set Google Text-To-Speech audio configuration parameters
-    	audio_config = texttospeech.types.AudioConfig( 
-        	audio_encoding=texttospeech.enums.AudioEncoding.MP3, 
-		effects_profile_id=[effects_profile_id])
+    	audio_config = texttospeech.types.AudioConfig(audio_encoding=texttospeech.enums.AudioEncoding.MP3, effects_profile_id=[effects_profile_id])
 
 	# Request speech synthesis from Google Text-To-Speech
     	response = client.synthesize_speech(input_text, voice, audio_config)
