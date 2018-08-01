9062abef2d08407500fa01f32fcf6b08873e26f5
vijayraghu <vijayasarathypr@gmail.com>
2018-08-01 16:20:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def dialogflow_text_to_intent(project_id, call_id, input_text, lang_code):
 		response = session_client.detect_intent(session=session, query_input=query_input)
 		print response
 		jsonObj = MessageToJson(response.query_result)
-		output = json.loads(jsonObj)
+		output = json.loads(jsonObj)["parameters"]
 		
 		# Return properties from Dialogflow
 		try:
